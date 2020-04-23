ea46ad2a1b86cbf292e1c049c712cc396aed5f53
Karl Kedrovsky <Karl.Kedrovsky@vmlyr.com>
2020-04-23 14:34:19 -0500

    added icons

--- src/main.js
+++ src/main.js
@@ -5,8 +5,17 @@ require('typeface-raleway');
 require('~/main.css');
 
 import DefaultLayout from '~/layouts/Default.vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { config, library } from '@fortawesome/fontawesome-svg-core';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
+config.autoAddCss = false;
+library.add(faArrowRight);
 
 export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout), (head.htmlAttrs = { class: 'h-full', style: 'overflow-y: scroll' }), (head.bodyAttrs = { class: 'h-full flex flex-col' });
+  // Set font-awesome component globally
+  Vue.component('font-awesome', FontAwesomeIcon);
 }
