b053fd64de2424526d51f798f56e003d3cb7632c
Karl Kedrovsky <Karl.Kedrovsky@vmlyr.com>
2020-04-26 08:48:57 -0500

    added gtag support for google analytics

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a simple static site generated using Gridsome.
 
-# Installation and Build
+## Installation and Build
 
 The following steps are the standard steps for building a Gridsome site. The should be run from the top level directory of this repo.
 
@@ -10,3 +10,10 @@ The following steps are the standard steps for building a Gridsome site. The sho
 2. `npm install` to install all of the project dependencies.
 3. `gridsome develop` to run a local development server.
 4. `gridsome build` to generate the site for deployment (in the dist directory).
+
+## Build Notes
+
+There are two environment variables that the build uses when generating the site.
+
+- FONTAWESOME_NPM_AUTH_TOKEN - This is the token required to use the pro version of Fontawesome. You can get it from your [account](https://fontawesome.com/account) page after you've registered for a pro account.
+- GRIDSOME_GTAG_ID - The "UA-XXXXX" Google GTag/Analytics ID.
--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import { config, library } from '@fortawesome/fontawesome-svg-core';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { faLongArrowRight } from '@fortawesome/pro-light-svg-icons';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import VueGtag from 'vue-gtag';
 
 config.autoAddCss = false;
 library.add(faArrowRight, faLongArrowRight);
@@ -19,6 +20,15 @@ export default function(Vue, { router, head, isClient }) {
   Vue.component('Layout', DefaultLayout),
     (head.htmlAttrs = { class: 'h-full text-kc-covid-black', style: 'overflow-y: scroll' }),
     (head.bodyAttrs = { class: 'h-full flex flex-col' });
+
   // Set font-awesome component globally
   Vue.component('font-awesome', FontAwesomeIcon);
+
+  // Add Google GTag for analytics
+  if (process.env.GRIDSOME_GTAG_ID) {
+    console.log('adding gtag');
+    Vue.use(VueGtag, {
+      config: { id: process.env.GRIDSOME_GTAG_ID }
+    });
+  }
 }
