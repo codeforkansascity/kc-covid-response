83e7440d7186284ccd344912ac6b30c39435f049
Karl Kedrovsky <Karl.Kedrovsky@vmlyr.com>
2020-05-03 17:08:14 -0500

    fixed mobile screen layout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -82,7 +82,7 @@
         </div>
       </template>
       <template v-slot:column2>
-        <div class="px-4 md:px-16 mb-8 md:mb-0 relative h-72 md:h-full flex justify-center overflow-hidden">
+        <div class="px-4 md:px-16 mb-8 md:mb-0 relative h-72 md:h-full flex md:block justify-center overflow-hidden">
           <g-image class="absolute md:-bottom-32 md:left-76" src="~/images/safe-paths-app-screen-2.png" />
           <g-image class="absolute md:bottom-0 md:left-16" src="~/images/safe-paths-app-screen-1.png" />
         </div>
