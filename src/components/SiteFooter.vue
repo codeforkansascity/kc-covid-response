3231456754c385be00d950bbdcf0331dd530d784
Karl Kedrovsky <Karl.Kedrovsky@vmlyr.com>
2020-05-03 19:04:05 -0500

    fixed hydration problem with footer links

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="h-32 container mx-auto content-center">
     <div class="h-full px-4 text-white flex flex-col md:flex-row justify-around md:justify-between items-center">
-      <ul class="flex items-center">
-        <li>
-          <a href="https://twitter.com/comebackkc" class="underline" target="_blank"><font-awesome class="mr-4" :icon="['fab', 'twitter']" size="lg"></font-awesome></a>
-        </li>
-        <li>
-          <a href="https://www.facebook.com/Comeback-KC-107935990899809/" class="underline" target="_blank"
-            ><font-awesome class="mr-4" :icon="['fab', 'facebook']" size="lg"></font-awesome
-          ></a>
-        </li>
-        <li>
-          <a href="https://www.instagram.com/comeback_kc/" class="underline" target="_blank"><font-awesome class="mr-4" :icon="['fab', 'instagram']" size="lg"></font-awesome></a>
-        </li>
-      </ul>
-      <ul class="flex flex-col md:flex-row justify-between items-center">
-        <li><g-link to="/privacy-policy/" class="font-bold uppercase underline mr-4">Privacy Policy</g-link></li>
-        <li><g-link to="/terms-conditions/" class="font-bold uppercase underline">Terms and Conditions</g-link></li>
-      </ul>
+      <div>
+        <ul class="flex items-center">
+          <li>
+            <a href="https://twitter.com/comebackkc" class="underline" target="_blank"><font-awesome class="mr-4" :icon="['fab', 'twitter']" size="lg"></font-awesome></a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/Comeback-KC-107935990899809/" class="underline" target="_blank"
+              ><font-awesome class="mr-4" :icon="['fab', 'facebook']" size="lg"></font-awesome
+            ></a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/comeback_kc/" class="underline" target="_blank"><font-awesome class="mr-4" :icon="['fab', 'instagram']" size="lg"></font-awesome></a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul class="flex flex-col md:flex-row justify-between items-center">
+          <li><g-link to="/privacy-policy/" class="font-bold uppercase underline mr-4">Privacy Policy</g-link></li>
+          <li><g-link to="/terms-conditions/" class="font-bold uppercase underline">Terms and Conditions</g-link></li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
