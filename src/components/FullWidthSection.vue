eb739fd83f576e0cbc5f24d372743bf501b137b8
Karl Kedrovsky <Karl.Kedrovsky@vmlyr.com>
2020-05-30 08:35:25 -0500

    Added background image for hero space

--- src/components/FullWidthSection.vue
+++ src/components/FullWidthSection.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="w-full bg-center bg-cover" :style="{ backgroundImage: backgroundImage }">
+  <div class="relative w-full overflow-hidden bg-center bg-cover" :style="{ backgroundImage: backgroundImage }">
+    <video v-if="backgroundVideo" autoplay loop class="absolute min-w-full min-h-full transform -translate-x-1/2 -translate-y-1/2 -z-10 top-1/2 left-1/2">
+      <source :src="backgroundVideo" type="video/mp4">
+    </video>
     <slot></slot>
   </div>
 </template>
@@ -7,11 +10,15 @@
 <script>
 export default {
   props: {
-    image: String
+    image: String,
+    video: String
   },
   computed: {
     backgroundImage() {
       return this.image ? 'url(' + this.image + ')' : 'none'
+    },
+    backgroundVideo() {
+      return this.video ? this.video : false
     }
   }
 }
