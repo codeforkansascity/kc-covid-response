623b980281c8102b834db817ff7a294998e26106
Karl Kedrovsky <Karl.Kedrovsky@vmlyr.com>
2020-06-02 11:27:14 -0500

    Added poster attribute to video tag

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout language="en">
-    <PageHeader video="/videos/hero.mp4">
+    <PageHeader video="/videos/hero.mp4" image="/images/kc-skyline-bg.jpg">
       <template v-slot:sub-title>
         Test. Track. <span class="text-comebackkc-darkblue">Triumph.</span>
       </template>
