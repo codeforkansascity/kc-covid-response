6b5ff990ffdf7fef0b2880dfab6ca61965d75037
Karl Kedrovsky <Karl.Kedrovsky@vmlyr.com>
2020-06-03 20:38:02 -0500

    Added muted attribute to video tag

--- src/components/FullWidthSection.vue
+++ src/components/FullWidthSection.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative w-full overflow-hidden bg-center bg-cover" :style="{ backgroundImage: backgroundImage }">
-    <video v-if="backgroundVideo" autoplay loop class="absolute min-w-full min-h-full transform -translate-x-1/2 -translate-y-1/2 -z-10 top-1/2 left-1/2">
+    <video v-if="backgroundVideo" autoplay loop muted class="absolute min-w-full min-h-full transform -translate-x-1/2 -translate-y-1/2 -z-10 top-1/2 left-1/2">
       <source :src="backgroundVideo" type="video/mp4" :poster="backgroundVideoPoster">
     </video>
     <slot></slot>
